c37e143fc41089691c6cbca7b27a97b34d5415a9
lucas quivron <lucas.quivron@gmail.com>
2016-08-12 13:19:46 +0200

    Add figaro - Protect my secret data in config/application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+cocktails_attributes = [
+  {
+    name:"Cuba Loco"
+  },
+  {
+    name:"White Russian"
+
+  },
+  {
+    name:"Green Lantern"
+  }
+]
+cocktails_attributes.each { |params| Cocktail.create!(params) }
+
+
+Ingredient.destroy_all
+ingredients_attributes = [
+  {
+    name:"vodka"
+  },
+  {
+    name:"whisky"
+  },
+  {
+    name:"Rhum"
+  },
+  {
+    name:"minth"
+  },
+  {
+    name:"lemon"
+    },
+  {
+    name:"coke"
+  },
+  {
+    name:"perrier"
+  },
+  {
+    name:"tequila"
+  },
+  {
+    name:"milk"
+  },
+  {
+    name:"ice"
+  }
+]
+ingredients_attributes.each { |params| Ingredient.create!(params) }
